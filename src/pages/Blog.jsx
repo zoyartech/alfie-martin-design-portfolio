97d4bedbdba04923e95df6853078d6728c609837
Ally M <nycuxdesign@gmail.com>
2026-03-01 04:09:41 +0000

    File changes

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { motion } from "framer-motion";
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Bookmark } from "lucide-react";
 import { blogPosts } from "../components/blog/blogData";
 import AIBlogAssistant from "../components/blog/AIBlogAssistant";
 
