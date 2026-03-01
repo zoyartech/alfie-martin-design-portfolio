639d8861115ebecee97caf1b8eb4f557dbd0f89d
Ally M <nycuxdesign@gmail.com>
2026-03-01 02:45:03 +0000

    File changes

--- src/pages/DesignStrategyAI.jsx
+++ src/pages/DesignStrategyAI.jsx
@@ -221,7 +221,7 @@ export default function DesignStrategyAI() {
             transition={{ duration: 0.7 }}
             className="aspect-[16/7] overflow-hidden bg-gray-100">
             <img
-              src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6974e154f708f4918a2b8d02/ee3793a4d_Screenshot2026-02-22at71120PM.png"
+              src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6974e154f708f4918a2b8d02/c5fa5897d_Screenshot2026-02-28at94344PM.png"
               alt="Six principles for chatbot design"
               className="w-full h-full object-cover" />
 
